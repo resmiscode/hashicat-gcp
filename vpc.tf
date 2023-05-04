374c07405386b6df2b15cb9c664f127c6b7eae31
ResmiK <kartha.resmi@gmail.com>
2023-05-04 21:42:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,14 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = var.project
-    network_name = "gaurav-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
-        }
-    ]
-
-    
-
-    
+module "network" {
+  source  = "app.terraform.io/resmi-org/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  project_id   = var.project_id
+  network_name = "gaurav-network"
+  subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+  ]
 }
