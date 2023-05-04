45f3484a24dae5856e4a5d02fdb45ca9875df5e3
ResmiK <kartha.resmi@gmail.com>
2023-05-04 19:05:20 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -51,11 +51,6 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
 
-  labels = {
-    "department" = "devops",
-    "billable" = "true"
-  }
-
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
