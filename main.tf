3a9722450888b0e3161bc29531a554238a2e092e
ResmiK <kartha.resmi@gmail.com>
2023-05-04 18:54:44 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -52,7 +52,8 @@ resource "google_compute_instance" "hashicat" {
   machine_type = var.machine_type
 
   labels = {
-    department = "devops"
+    department = "devops",
+    billable = "true"
   }
 
   boot_disk {
